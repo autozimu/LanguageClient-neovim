d0768634efb50401142c87d21f1eb5f06d878694
Jeff Li <junfengl@ca.ibm.com>
2017-01-25 14:33:23 -0500

    IntegrationTest!

--- LanguageClient/LanguageClient_test.py
+++ LanguageClient/LanguageClient_test.py
@@ -9,53 +9,40 @@ NVIM_LISTEN_ADDRESS = "/tmp/nvim-LanguageClient-IntegrationTest"
 PROJECT_ROOT_PATH = joinPath("tests/sample-rs")
 MAINRS_PATH = joinPath("tests/sample-rs/src/main.rs")
 
-
 @pytest.fixture(scope="module")
 def nvim() -> neovim.Nvim:
     nvim = neovim.attach('socket', path=NVIM_LISTEN_ADDRESS)
-    nvim.command("edit {}".format(MAINRS_PATH))
+    time.sleep(0.1)
+    nvim.command("edit! {}".format(MAINRS_PATH))
+    nvim.command("LanguageClientStart")
+    nvim.command("call LanguageClient_initialize()")
+    nvim.command("call LanguageClient_textDocument_didOpen()")
+    time.sleep(2)
     return nvim
 
-def test_start(nvim):
-    nvim.command("LanguageClientStart")
-
-def test_initialize(nvim):
-    nvim.command("call LanguageClient_initialize()")
-
-def test_textDocument_didOpen(nvim):
-    nvim.command("call LanguageClient_textDocument_didOpen()")
-
-def test_textDocument_didOpen(nvim):
-    nvim.command("call LanguageClient_textDocument_didOpen()")
-
 def test_textDocument_hover(nvim):
-    nvim.command('normal! 9G23|')
-    nvim.command('call LanguageClient_textDocument_hover()')
-    time.sleep(3)
-    print(nvim.command_output("messages"))
+    nvim.command("normal! 9G23|")
+    nvim.command("redir => g:echo")
+    nvim.command("call LanguageClient_textDocument_hover()")
+    time.sleep(0.2)
+    nvim.command("redir END")
+    assert nvim.eval("g:echo").strip() == "fn () -> i32"
 
-#     def test_textDocument_hover(self):
-#         self.client.textDocument_hover(
-#                 [joinPath("tests/sample-rs/src/main.rs"), 8, 22],
-#                 lambda sign: assertEqual(sign, 'fn () -> i32'))
-#         self.waitForResponse(5)
+def test_textDocument_definition(nvim):
+    nvim.command("normal! 9G23|")
+    nvim.command("call LanguageClient_textDocument_definition()")
+    time.sleep(0.2)
+    _, line, character, _ = nvim.eval("getpos('.')")
+    assert [line, character] == [3, 4]
 
-#     def test_textDocument_definition(self):
-#         self.client.textDocument_definition(
-#                 [joinPath("tests/sample-rs/src/main.rs"), 8, 22],
-#                 lambda loc:  assertEqual(loc, [3, 4]))
-#         self.waitForResponse(5)
+def test_textDocument_rename(nvim):
+    bufferContent = str.join("\n", nvim.eval("getline(1, '$')"))
+    nvim.command("normal! 9G23|")
+    nvim.command("call LanguageClient_textDocument_rename('hello')")
+    time.sleep(0.1)
+    updatedBufferContent = str.join("\n", nvim.eval("getline(1, '$')"))
+    assert updatedBufferContent == bufferContent.replace("greet", "hello")
+    nvim.command("edit! {}".format(MAINRS_PATH))
 
-#     def test_textDocument_rename(self):
-#         self.client.textDocument_rename(
-#                 [joinPath("tests/sample-rs/src/main.rs"), 8, 22, "hello"]
-#                 )
-#         # TODO: assert changes
-#         self.waitForResponse(5)
-
-#     def test_textDocument_documentSymbol(self):
-#         self.client.textDocument_documentSymbol(
-#                 [joinPath("tests/sample-rs/src/main.rs")]
-#                 )
-#         # TODO: assert changes
-#         self.waitForResponse(5)
+def test_textDocument_documentSymbol(nvim):
+    nvim.command("call LanguageClient_textDocument_documentSymbol()")
