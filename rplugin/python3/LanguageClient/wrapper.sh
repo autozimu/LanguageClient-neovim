70c72a0ce246c86bd12c535f39becf72c98c2a16
Junfeng Li <autozimu@gmail.com>
2017-02-09 11:39:49 -0500

    Add full version of log.

--- rplugin/python3/LanguageClient/wrapper.sh
+++ rplugin/python3/LanguageClient/wrapper.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 RUST_LOG=rls cargo run --manifest-path=/opt/rls/Cargo.toml 2>>/tmp/client.log
+
+# tee -a /tmp/client.log | RUST_LOG=rls cargo run --manifest-path=/opt/rls/Cargo.toml 2>>/tmp/client.log | tee -a /tmp/client.log
