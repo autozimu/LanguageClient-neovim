4aaa4935bd53368073034f7050d8f5c4092aa25b
Junfeng Li <autozimu@gmail.com>
2017-02-11 15:31:54 -0500

    Use shell script to drive test.
    As it because more and more complex.

---
+++ rplugin/python3/test.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+rm -f /tmp/nvim-LanguageClient-IntegrationTest
+cat /dev/null > /tmp/LanguageClient.log
+rm -rf LanguageClient/__pycache__
+
+nvim +UpdateRemotePlugins +qall
+NVIM_LISTEN_ADDRESS=/tmp/nvim-LanguageClient-IntegrationTest nvim -n -u tests/vimrc --headless 2>/dev/null &
+PID=$!
+sleep 1s
+
+py.test --capture=no --exitfirst
+ret=$?
+cat /tmp/LanguageClient.log
+
+kill $PID
+exit $ret
