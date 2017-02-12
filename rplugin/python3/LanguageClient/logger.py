28f7a5fa2875d4565b39a12802f1a41ea9e7f1ca
Junfeng Li <autozimu@gmail.com>
2017-02-12 11:40:32 -0500

    Shorten logging entry.

--- rplugin/python3/LanguageClient/logger.py
+++ rplugin/python3/LanguageClient/logger.py
@@ -1,9 +1,10 @@
 import logging
 
-logger = logging.getLogger('LanguageClient')
-fileHandler = logging.FileHandler(filename='/tmp/LanguageClient.log')
+logger = logging.getLogger("LanguageClient")
+fileHandler = logging.FileHandler(filename="/tmp/LanguageClient.log")
 fileHandler.setFormatter(
         logging.Formatter(
-            '%(asctime)s %(levelname)-8s (%(name)s) %(message)s'))
+            "%(asctime)s %(levelname)-8s %(message)s",
+            "%H:%M:%S"))
 logger.addHandler(fileHandler)
 logger.setLevel(logging.ERROR)
