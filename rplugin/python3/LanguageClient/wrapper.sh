8ba4714329224e961296580f6afe13f86ecacd87
Junfeng Li <autozimu@gmail.com>
2017-02-13 15:25:25 -0500

    Add php language server wrapper command.
    Might help with future debugging.

--- rplugin/python3/LanguageClient/wrapper.sh
+++ rplugin/python3/LanguageClient/wrapper.sh
@@ -3,3 +3,5 @@
 RUST_LOG=rls cargo run --manifest-path=/opt/rls/Cargo.toml 2>>/tmp/client.log
 
 # tee -a /tmp/client.log | RUST_LOG=rls cargo run --manifest-path=/opt/rls/Cargo.toml 2>>/tmp/client.log | tee -a /tmp/client.log
+
+# tee -a /tmp/LanguageClient.log | php $HOME/.config/composer/vendor/felixfbecker/language-server/bin/php-language-server.php | tee -a /tmp/LanguageClient.log
