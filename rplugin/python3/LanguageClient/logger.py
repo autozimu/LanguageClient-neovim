8ef801eb4bb62a89032698950621a1425e5e195c
Junfeng Li <autozimu@gmail.com>
2017-04-15 00:33:08 -0400

    Change default logging level.

--- rplugin/python3/LanguageClient/logger.py
+++ rplugin/python3/LanguageClient/logger.py
@@ -7,4 +7,4 @@ fileHandler.setFormatter(
             "%(asctime)s %(levelname)-8s %(message)s",
             "%H:%M:%S"))
 logger.addHandler(fileHandler)
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.WARN)
