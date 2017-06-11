e5a1c587a1eae3854d1d12467a818818f2e4091d
Jordan Patterson <jordan@evolarium.ca>
2017-06-11 16:07:42 -0600

    Use tempfile lib for log file
    The fixed name for the log file causes issues for multiple users.  The
    tempfile library will create temporary files that don't conflict.

--- rplugin/python3/LanguageClient/logger.py
+++ rplugin/python3/LanguageClient/logger.py
@@ -1,7 +1,12 @@
 import logging
+import tempfile
 
 logger = logging.getLogger("LanguageClient")
-fileHandler = logging.FileHandler(filename="/tmp/LanguageClient.log")
+with tempfile.NamedTemporaryFile(
+        prefix="LanguageClient-",
+        suffix=".log", delete=False) as tmp:
+    tmpname = tmp.name
+fileHandler = logging.FileHandler(filename=tmpname)
 fileHandler.setFormatter(
     logging.Formatter(
         "%(asctime)s %(levelname)-8s %(message)s",
