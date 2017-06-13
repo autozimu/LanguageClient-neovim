838020ecf2a9b0977b9b4ad7fcf371eff7d27c50
Junfeng Li <autozimu@gmail.com>
2017-06-13 18:53:05 +0800

    Fix denite sources filepath.
    Close #57.

--- rplugin/python3/LanguageClient/__init__.py
+++ rplugin/python3/LanguageClient/__init__.py
@@ -1,3 +1,4 @@
 from . LanguageClient import LanguageClient  # noqa: F401
 from . logger import logger  # noqa: F401
 from . CompletionItemKind import CompletionItemKind  # noqa: F401
+from . util import pathToURI  # noqa: F401
--- rplugin/python3/denite/source/references.py
+++ rplugin/python3/denite/source/references.py
@@ -7,21 +7,23 @@ LanguageClientPath = path.dirname(path.dirname(path.dirname(
     path.realpath(__file__))))
 # TODO: use relative path.
 sys.path.append(LanguageClientPath)
-from LanguageClient import LanguageClient  # noqa: E402
+from LanguageClient import LanguageClient, pathToURI  # noqa: E402
 
 
 class Source(Base):
     def __init__(self, vim):
         super().__init__(vim)
+        self.vim = vim
 
         self.name = 'references'
         self.kind = 'file'
 
     def convertToCandidate(self, locations) -> List[Dict]:
         candidates = []
+        pwd = pathToURI(self.vim.funcs.getcwd())
         for loc in locations:
             uri = loc["uri"]
-            filepath = path.relpath(uri, LanguageClient._instance.rootUri)
+            filepath = path.relpath(uri, pwd)
             start = loc["range"]["start"]
             line = start["line"] + 1
             character = start["character"] + 1
