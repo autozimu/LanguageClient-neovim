2a7ce1ac70f8767e9d2b2a9f1d335cfcc63a92b6
Junfeng Li <autozimu@gmail.com>
2017-06-13 18:16:36 +0800

    Revert "Use tempfile lib for log file"
    This reverts commit 6e8f35b83fc563c8349cb3be040c61a0588ca745.
    
    The commit caused severer issue than it fixed. In case one need to check
    the content of log file, there is no way to tell where the log file
    location/name is.

--- rplugin/python3/LanguageClient/logger.py
+++ rplugin/python3/LanguageClient/logger.py
@@ -1,12 +1,7 @@
 import logging
-import tempfile
 
 logger = logging.getLogger("LanguageClient")
-with tempfile.NamedTemporaryFile(
-        prefix="LanguageClient-",
-        suffix=".log", delete=False) as tmp:
-    tmpname = tmp.name
-fileHandler = logging.FileHandler(filename=tmpname)
+fileHandler = logging.FileHandler(filename="/tmp/LanguageClient.log")
 fileHandler.setFormatter(
     logging.Formatter(
         "%(asctime)s %(levelname)-8s %(message)s",
