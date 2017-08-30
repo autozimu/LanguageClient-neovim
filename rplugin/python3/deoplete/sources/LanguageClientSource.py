e63a471715371e03cbc49300d7f985d10ad14730
Herrington Darkholme <nonamesheep1@gmail.com>
2017-08-31 01:13:14 +0800

    defend for null sortText

--- rplugin/python3/deoplete/sources/LanguageClientSource.py
+++ rplugin/python3/deoplete/sources/LanguageClientSource.py
@@ -77,7 +77,7 @@ class Source(Base):
                 del self.__results[contextid]
                 if isinstance(items, dict):
                     items = items["items"]
-                items = sorted(items, key=lambda x: x.get("sortText"))
+                items = sorted(items, key=lambda x: x.get("sortText", x["label"]))
                 return [self.convertToDeopleteCandidate(item)
                         for item in items]
         else:  # send request
