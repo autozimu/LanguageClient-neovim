c523c88dd95e187d4895df2a0712b622d626c9d7
Junfeng Li <lijunfen@amazon.com>
2017-09-20 18:51:48 +0000

    RotatingFileHandler.
    Close #131.

--- rplugin/python3/LanguageClient/logger.py
+++ rplugin/python3/LanguageClient/logger.py
@@ -1,10 +1,11 @@
 import logging
+import logging.handlers
 import os
 
 logger = logging.getLogger("LanguageClient")
 logpath = os.path.join(os.getenv("TMP", "/tmp"), "LanguageClient.log")
 logpath_server = os.path.join(os.getenv("TMP", "/tmp"), "LanguageServer.log")
-fileHandler = logging.FileHandler(logpath)
+fileHandler = logging.handlers.RotatingFileHandler(logpath, maxBytes=1024 ** 20, backupCount=2)
 fileHandler.setFormatter(
     logging.Formatter(
         "%(asctime)s %(levelname)-7s [%(threadName)-10s] %(message)s",
