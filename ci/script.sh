fa56ec2abce2029b53efa605dadd1a7168656205
Junfeng Li <autozimu@gmail.com>
2017-11-26 11:07:37 -0800

    Build travis based on trust.

---
+++ ci/script.sh
@@ -0,0 +1,22 @@
+# This script takes care of testing your crate
+
+set -ex
+
+main() {
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    # TODO: run integration tests
+}
+
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
