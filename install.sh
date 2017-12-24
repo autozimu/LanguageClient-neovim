9e71f64466fb8636d86f12c542608c3cdae380c8
Junfeng Li <autozimu@gmail.com>
2017-12-23 22:03:31 -0800

    (cargo-release) version 0.1.8

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 set -o pipefail
 
-version=0.1.7
+version=0.1.8
 name=languageclient
 
 function try_curl() {
