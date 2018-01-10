f5a1e019d3f3ac3206f8a4fd6f5f2ba12097c17a
Junfeng Li <autozimu@gmail.com>
2018-01-09 22:40:16 -0800

    (cargo-release) version 0.1.25

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$version = '0.1.24'
+$version = '0.1.25'
 $name = 'languageclient'
 $url = "https://github.com/autozimu/LanguageClient-neovim/releases/download/$version/"
 $filename = "$name-$version-"
