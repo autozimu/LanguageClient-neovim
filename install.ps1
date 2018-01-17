ed183d77670538b4cdfbb9531a88e188e279b053
Junfeng Li <lijunfen@amazon.com>
2018-01-17 02:47:42 +0000

    (cargo-release) version 0.1.28

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$version = '0.1.27'
+$version = '0.1.28'
 $name = 'languageclient'
 $url = "https://github.com/autozimu/LanguageClient-neovim/releases/download/$version/"
 $filename = "$name-$version-"
--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 set -o pipefail
 
-version=0.1.27
+version=0.1.28
 name=languageclient
 
 function try_curl() {
