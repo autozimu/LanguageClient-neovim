bda4700815655966767b0507e0a8f38013a96cc2
Junfeng Li <lijunfen@amazon.com>
2018-01-25 11:36:10 -0800

    Cleanup ps1.

--- install.ps1
+++ install.ps1
@@ -3,19 +3,19 @@ $name = 'languageclient'
 $url = "https://github.com/autozimu/LanguageClient-neovim/releases/download/$version/"
 $filename = "$name-$version-"
 
-if ($ENV:PROCESSOR_ARCHITECTURE -eq "AMD64") {
-    $filename = $filename + 'x86_64'
+if ($ENV:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
+    $filename += 'x86_64'
 } else {
-    $filename = $filename + 'i686'
+    $filename += 'i686'
 }
 
-$filename = $filename + '-pc-windows-gnu.exe'
-$url = $url + $filename
+$filename += '-pc-windows-gnu.exe'
+$url += $filename
 
 $path = "bin\$name.exe"
-if (Test-Path "$path") {
-    Remove-Item -Force "$path"
+if (Test-Path $path) {
+    Remove-Item -Force $path
 }
-$downloader = new-object System.Net.WebClient
-echo "Downloading $url"
+$downloader = New-Object System.Net.WebClient
+echo "Downloading $url ..."
 $downloader.DownloadFile($url, $path)
