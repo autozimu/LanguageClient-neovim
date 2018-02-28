4b80eafa30626ef8f1a29ee2dce5660dde76b217
Yatao Li <yatli@microsoft.com>
2018-02-28 14:16:45 +0800

    powershell installation script use TLS 1.2 explictly (#318)

--- install.ps1
+++ install.ps1
@@ -14,6 +14,6 @@ $path = "bin\$name.exe"
 if (Test-Path $path) {
     Remove-Item -Force $path
 }
-$downloader = New-Object System.Net.WebClient
 echo "Downloading $url ..."
-$downloader.DownloadFile($url, $path)
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $url -OutFile $path
