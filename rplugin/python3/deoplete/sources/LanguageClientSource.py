fbc46862af7fa254f74f1108149fd0669c46f1ad
Junfeng Li <autozimu@gmail.com>
2018-03-06 22:44:13 -0800

    Remove problematic deoplete source customization.
    Close #312.

--- rplugin/python3/deoplete/sources/LanguageClientSource.py
+++ rplugin/python3/deoplete/sources/LanguageClientSource.py
@@ -1,14 +1,7 @@
-import re
 from .base import Base
 
 
 CompleteResults = "g:LanguageClient_completeResults"
-
-
-def simplify_snippet(snip: str) -> str:
-    snip = re.sub(r'(?<!\\)\$(?P<num>\d+)', '<`\g<num>`>', snip)
-    return re.sub(r'(?<!\\)\${(?P<num>\d+):(?P<desc>.+?)}',
-                  '<`\g<num>:\g<desc>`>', snip)
 
 
 class Source(Base):
@@ -20,21 +13,6 @@ class Source(Base):
         self.rank = 1000
         self.filetypes = vim.eval(
             "get(g:, 'LanguageClient_serverCommands', {})").keys()
-        self.min_pattern_length = 1
-        self.input_pattern = r'(\.|::|->)\w*'
-        self.__keyword_patterns = r'(?:[a-zA-Z@0-9_À-ÿ]|\.|::|->)*$'
-
-    def get_complete_position(self, context):
-        m = re.search('(?:' + context['keyword_patterns'] + ')$',
-                      context['input'])
-        if m:
-            return m.start()
-
-        m = re.search(self.__keyword_patterns, context['input'])
-        if m:
-            return m.end()
-
-        return -1
 
     def gather_candidates(self, context):
         if not context["is_async"]:
