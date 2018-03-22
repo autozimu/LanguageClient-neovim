39e8c4a018d33b58b78030d74f57131bbe86d10c
yuya yano <yuya.yano.6260@gmail.com>
2018-03-22 10:39:36 +0900

    Fix deoplete completion issue.
    Related to #281 and #329

--- rplugin/python3/deoplete/sources/LanguageClientSource.py
+++ rplugin/python3/deoplete/sources/LanguageClientSource.py
@@ -1,7 +1,8 @@
 from .base import Base
+import re
 
 
-CompleteResults = "g:LanguageClient_completeResults"
+CompleteResults = "g:LanguageClient_omniCompleteResults"
 
 
 class Source(Base):
@@ -11,19 +12,27 @@ class Source(Base):
         self.name = "LanguageClient"
         self.mark = "[LC]"
         self.rank = 1000
+        self.sorters = ["sorter_word"]
+        self.min_pattern_length = 1
         self.filetypes = vim.eval(
             "get(g:, 'LanguageClient_serverCommands', {})").keys()
+        self.input_pattern += r'(\.|::|->)\w*$'
+        self.complete_pos = re.compile(r"\w*$")
+
+    def get_complete_position(self, context):
+        m = self.complete_pos.search(context['input'])
+        return m.start() if m else -1
 
     def gather_candidates(self, context):
-        if not context["is_async"]:
+        if context["is_async"]:
+            results = self.vim.eval(CompleteResults)
+            if len(results) != 0:
+                context["is_async"] = False
+                return results[0]
+        else:
             context["is_async"] = True
-            self.vim.funcs.LanguageClient_omniComplete()
-            return []
-        elif self.vim.funcs.eval("len({})".format(CompleteResults)) == 0:
-            return []
-
-        context["is_async"] = False
-        result = self.vim.funcs.eval("remove({}, 0)".format(CompleteResults))
-        if not isinstance(result, list):
-            result = []
-        return result
+            self.vim.command("let {0} = []".format(CompleteResults))
+            self.vim.funcs.LanguageClient_omniComplete(
+                    {"character": context["complete_position"]}
+            )
+        return []
