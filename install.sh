d201d0ce9dc895ba6963c132fb53e436a6e701dc
Junfeng Li <lijunfen@amazon.com>
2018-04-24 17:17:44 -0700

    (cargo-release) version 0.1.72

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 set -o pipefail
 
-version=0.1.71
+version=0.1.72
 name=languageclient
 
 function try_curl() {
