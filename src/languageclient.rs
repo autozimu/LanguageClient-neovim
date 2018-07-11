a3c235cca810b1abcb0868e0f8816c39c6664514
roxma <roxma@qq.com>
2018-07-11 22:05:15 +0800

    skip handleCompleteDone if edits is empty, fix ncm2/ncm2#16

--- src/languageclient.rs
+++ src/languageclient.rs
@@ -2210,6 +2210,10 @@ impl State {
             edits.extend(aedits.clone());
         };
 
+        if edits.is_empty() {
+            return Ok(())
+        }
+
         self.apply_TextEdits(filename, &edits)?;
         self.cursor(line + 1, character + 1)
     }
