9e0a784365efe83ac3bf575747542e50d853d2f1
Junfeng Li <lijunfen@amazon.com>
2018-07-23 10:08:10 -0700

    Add purescript example project.

---
+++ tests/data/sample-purescript/src/Main.purs
@@ -0,0 +1,9 @@
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+main :: Effect Unit
+main = do
+  log "Hello sailor!"
---
+++ tests/data/sample-purescript/test/Main.purs
@@ -0,0 +1,9 @@
+module Test.Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+main :: Effect Unit
+main = do
+  log "You should add some tests."
