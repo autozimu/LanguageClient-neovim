1f5af57a9cab2b022b410438e84e2de267100b18
Junfeng Li <lijunfen@amazon.com>
2018-08-23 15:39:42 -0700

    Simplify to_url.

--- src/utils.rs
+++ src/utils.rs
@@ -120,14 +120,9 @@ pub trait ToUrl {
 
 impl<P: AsRef<Path> + std::fmt::Debug> ToUrl for P {
     fn to_url(&self) -> Result<Url> {
-        Url::from_file_path(self).or_else(|_| (
-            Url::from_str(&self.as_ref().to_string_lossy()).or_else(|_| {
-                Err(format_err!(
-                        "Failed to convert from path ({:?}) to Url",
-                        self
-                ))
-            })
-        ))
+        Url::from_file_path(self)
+            .or_else(|_| Url::from_str(&self.as_ref().to_string_lossy()))
+            .or_else(|_| Err(format_err!("Failed to convert ({:?}) to Url", self)))
     }
 }
 
