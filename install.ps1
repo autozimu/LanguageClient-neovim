3e182e7eccb8b1805d2e282cae38dfa1d157b6e0
Tyler James Leonhardt <tylerl0706@gmail.com>
2019-04-18 15:28:38 -0700

    Cross platform PowerShell install script (#794)
    * crossplat install.ps1
    
    * remove return
    
    * chmod +x install.ps1
    
    * handle ARM
    
    * switch to 1 switch
    
    * Space.

--- install.ps1
+++ install.ps1
@@ -1,19 +1,46 @@
+#!/usr/bin/env pwsh
+
 $version = '0.1.145'
 $name = 'languageclient'
 $url = "https://github.com/autozimu/LanguageClient-neovim/releases/download/$version/$name-$version-"
 
-if ($ENV:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
-    $url += 'x86_64'
-} else {
-    $url += 'i686'
+$path = "$PSScriptRoot\bin\$name"
+
+switch ($true) {
+    $IsMacOS {
+        # MacOS is always x86_64
+        $url += 'x86_64-apple-darwin'
+    }
+    $IsLinux {
+        # Detecting architecture is more involved on Linux
+        $arch = uname -sm
+        $url += switch ($arch) {
+            'Linux x86_64' { 'x86_64' }
+            'Linux i686' { 'i686' }
+            'Linux aarch64' { 'aarch64' }
+            Default { throw 'architecture not supported' }
+        }
+        $url += '-unknown-linux-musl'
+    }
+    Default {
+        # Windows
+        $url += if ([Environment]::Is64BitOperatingSystem) { 'x86_64' } else { 'i686' }
+        $url += '-pc-windows-gnu.exe'
+        
+        # We need to tack on the .exe to the end of the download path
+        $path += '.exe'
+    }
 }
 
-$url += '-pc-windows-gnu.exe'
-
-$path = "$PSScriptRoot\bin\$name.exe"
-if (Test-Path $path) {
-    Remove-Item -Force $path
+if (Test-Path -LiteralPath $path) {
+    Remove-Item -Force -LiteralPath $path
 }
+
 echo "Downloading $url ..."
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if (!$IsCoreCLR) {
+    # We only need to do this for Windows PowerShell
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+}
+
 Invoke-WebRequest -Uri $url -OutFile $path
