6eae39972b669df5611ed13ee66eb25ae2457ac2
Junfeng Li <autozimu@gmail.com>
2019-09-14 16:10:15 -0700

    (cargo-release) version 0.1.148

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-$version = '0.1.147'
+$version = '0.1.148'
 $name = 'languageclient'
 $url = "https://github.com/autozimu/LanguageClient-neovim/releases/download/$version/$name-$version-"
 
