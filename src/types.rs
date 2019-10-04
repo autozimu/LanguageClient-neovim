34e695dde1a4f27903ec06bbdce54b65c802fc0d
Junfeng Li <autozimu@gmail.com>
2019-10-03 21:32:08 -0700

    Try extract completion word from text_edit
    Close #857

--- src/types.rs
+++ src/types.rs
@@ -532,10 +532,30 @@ impl VimCompleteItem {
             lspitem, complete_position
         );
         let abbr = lspitem.label.clone();
-        let word = lspitem
-            .insert_text
-            .clone()
-            .unwrap_or_else(|| lspitem.label.clone());
+
+        let word = lspitem.insert_text.clone().unwrap_or_else(|| {
+            match (lspitem.text_edit.clone(), complete_position) {
+                (Some(ref text_edit), Some(complete_position))
+                    if !text_edit.new_text.is_empty() =>
+                {
+                    // TextEdit range start might be different from vim expected completion start.
+                    // From spec, TextEdit can only span one line, i.e., the current line.
+                    if text_edit.range.start.character != complete_position {
+                        text_edit
+                            .new_text
+                            .get((complete_position as usize)..)
+                            .and_then(|line| line.split_whitespace().next())
+                            .map_or_else(String::new, ToOwned::to_owned)
+                    } else {
+                        text_edit.new_text.clone()
+                    }
+                }
+                (Some(ref text_edit), _) if !text_edit.new_text.is_empty() => {
+                    text_edit.new_text.clone()
+                }
+                (_, _) => lspitem.label.clone(),
+            }
+        });
 
         let snippet;
         if lspitem.insert_text_format == Some(InsertTextFormat::Snippet) {
---
+++ tests/data/sample-cpp/completion.c
@@ -0,0 +1,8 @@
+struct Point {
+	int x;
+};
+
+int main() {
+	struct Point point;
+    point.
+}
