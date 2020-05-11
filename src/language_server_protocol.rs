bcc8acb5472781c3a8880f465f2940a8a509831d
Martin Asquino <martin.asquino@gmail.com>
2020-05-11 08:29:50 +0100

    Implement $/progress notification

--- src/language_server_protocol.rs
+++ src/language_server_protocol.rs
@@ -8,6 +8,7 @@ use crate::rpcclient::RpcClient;
 use failure::err_msg;
 use itertools::Itertools;
 use notify::Watcher;
+use serde::de::Deserialize;
 use std::sync::mpsc;
 use vim::try_get;
 
@@ -2062,6 +2063,36 @@ impl LanguageClient {
             .notify("s:FZF", json!([source?, NOTIFICATION__FZFSinkCommand]))?;
 
         Ok(Value::Null)
+    }
+
+    pub fn progress(&self, params: &Value) -> Fallible<()> {
+        let params = ProgressParams::deserialize(params)?;
+        let message = match params.value {
+            ProgressParamsValue::WorkDone(wd) => match wd {
+                WorkDoneProgress::Begin(r) => {
+                    Some(format!("{} {}", r.title, r.message.unwrap_or_default()))
+                }
+                WorkDoneProgress::Report(r) => r.message,
+                // WorkDoneProgress::End has no value, so we return Done, otherwise the previous
+                // message would be left in screen and it would appear as if it didn't ever finish.
+                WorkDoneProgress::End(_) => Some("Done".into()),
+            },
+        };
+
+        if message.is_none() {
+            return Ok(());
+        }
+
+        let token = match params.token {
+            // number is a not a particularly useful token to report to the user, so we just use
+            // INFO instead.
+            NumberOrString::Number(_) => "INFO".to_string(),
+            NumberOrString::String(s) => s,
+        };
+
+        let message = format!("{}: {}", token, message.unwrap_or_default());
+        self.vim()?.echomsg(&message)?;
+        Ok(())
     }
 
     pub fn textDocument_codeLens(&self, params: &Value) -> Fallible<Value> {
