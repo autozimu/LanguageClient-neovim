3c2db876964f70167b646ac92098d720f048297e
Junfeng Li <autozimu@gmail.com>
2020-09-26 01:33:12 -0700

    First test

--- tests/integ.rs
+++ tests/integ.rs
@@ -1,25 +1,51 @@
+use lexpr::sexp;
 use once_cell::sync::Lazy;
 use std::path::PathBuf;
+use std::time::Duration;
 use vil::Vim;
-use lexpr::sexp;
 
 static VIM: Lazy<Vim> = Lazy::new(|| Vim::new().expect("Failed to create VIM"));
 
 static ROOT: Lazy<PathBuf> = Lazy::new(|| "tests/data/sample-rs".into());
+
+/// Assert with timeout.
+fn assert_eq_timeout<T>(f: fn() -> T, expected: T, timeout: Duration)
+where
+    T: std::cmp::PartialEq + std::fmt::Debug,
+{
+    let now = std::time::Instant::now();
+
+    loop {
+        let actual = f();
+        if actual == expected {
+            break;
+        }
+        if now.elapsed() > timeout {
+            // Get better error message.
+            assert_eq!(actual, expected);
+        }
+
+        std::thread::sleep(Duration::from_millis(100));
+    }
+}
 
 #[test]
 fn test_text_document_definition() {
     VIM.edit(&ROOT.join("src/main.rs")).unwrap();
     VIM.cursor(3, 22).unwrap();
 
-    let _: String = VIM.eval(
-        sexp!(
+    let _: String = VIM
+        .eval(sexp!(
             (vimcall "LanguageClient#textDocument_definition")
-        )
-    ).unwrap();
+        ))
+        .unwrap();
 
-    std::thread::sleep_ms(3000);
-    let curpos = VIM.getcurpos().unwrap();
-    assert_eq!(curpos.lnum, 8);
-    assert_eq!(curpos.col, 4);
+    assert_eq_timeout(
+        || {
+            let curpos = VIM.getcurpos().unwrap();
+            (curpos.lnum, curpos.col)
+        },
+        (8, 4),
+        Duration::from_secs(5),
+    );
 }
