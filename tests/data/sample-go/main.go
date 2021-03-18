70456d447d5e80f47e2d3ae2f948cab9ffe355dc
martskins <martskins@users.noreply.github.com>
2021-03-18 21:58:51 +0000

    Fix tests and update toolchain to latest release (#1203)

---
+++ tests/data/sample-go/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello, 世界")
+	log.Println(greet())
+}
+
+func greet() int32 {
+	return 42
+}
+
+func yo() {
+	someVar := 3
+	b := someVar + someVar
+	log.Println(b)
+	log.Println(otherYo())
+}
---
+++ tests/data/sample-go/other.go
@@ -0,0 +1,8 @@
+package main
+
+import "log"
+
+func otherYo() string {
+	log.Println(greet())
+	return "yo"
+}
